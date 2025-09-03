fdeec4f8a454d1e3f0b5266c8cfead7e76cf73a9
Thananan <thananan.pl@gmail.com>
2025-09-03 22:12:18 +0700

    chore: declare common types for account and use account service in auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,7 @@
 // auth.module.ts
 import { Module } from "@nestjs/common"
 import { AuthService } from "./auth.service"
-import { UsersModule } from "../users/users.module"
+import { AccountModule } from "src/modules/account/account.module"
 import { JwtModule } from "@nestjs/jwt"
 import { AuthController } from "./auth.controller"
 import { ConfigModule, ConfigService } from "@nestjs/config"
@@ -9,7 +9,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config"
 // TO DO: include this module in app module to use
 @Module({
   imports: [
-    UsersModule,
+    AccountModule,
     ConfigModule.forRoot({ isGlobal: true }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,11 @@
 // auth.service.ts
 import { Injectable, UnauthorizedException } from "@nestjs/common"
-import { UsersService } from "../users/users.service"
+import { AccountService } from "src/modules/account/account.service"
 import { comparePassword } from "src/utils/hash.util"
 import { JwtService } from "@nestjs/jwt"
 import { ConfigService } from "@nestjs/config"
 import { LoginResult } from "./interface/login-result.interface"
+import { Account } from "src/common/types/account.types"
 
 type JwtPayload = {
   sub: string
@@ -16,16 +17,16 @@ type JwtPayload = {
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    private accountService: AccountService,
     private jwtService: JwtService,
     private config: ConfigService
   ) {}
 
   async login(username: string, pass: string): Promise<LoginResult> {
-    const user = await this.usersService.findByUsername(username)
+    const user: Account = await this.accountService.getAccountByUsername(username)
     if (!user) throw new UnauthorizedException()
 
-    const ok = await comparePassword(pass, user.password)
+    const ok = await comparePassword(pass, user.passwordHash)
     if (!ok) throw new UnauthorizedException()
 
     const safeUser = {

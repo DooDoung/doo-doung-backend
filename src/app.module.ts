813734543b7fa4bee961a7c3643f683b6cbca23d
wnmay <wanathachat@gmail.com>
2025-09-07 01:28:28 +0700

    add: create review module with get review by account id logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common"
 import { PrismaModule } from "./db/prisma.module"
 import { LoggerMiddleware } from "./common/middleware/logger.middleware"
 import { AccountModule } from "./modules/account/account.module"
+import { ReviewModule } from "./modules/review/review.module"
 
 @Module({
-  imports: [PrismaModule, AccountModule],
+  imports: [PrismaModule, AccountModule, ReviewModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/modules/review/review.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from "@nestjs/common"
+import { ReviewRepository } from "./review.repository"
+import { CustomerService } from "../customer/customer.service"
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    private readonly repo: ReviewRepository,
+    private readonly customerService: CustomerService
+  ) {}
+
+  async getReviewByAccountId(accountId: string) {
+    const customer =
+      await this.customerService.getCustomerByAccountId(accountId)
+    if (!customer?.id) {
+      throw new NotFoundException("Customer not found")
+    }
+
+    //Todo: add validate own account
+    if (customer.isPublic) {
+      const reviewData = await this.repo.findByAccountId(customer.id)
+      const review = reviewData.map(r => ({
+        score: r.score,
+        description: r.description,
+        courseName: r.booking.course.courseName,
+      }))
+      return review
+    } else {
+      return { reviews: [] }
+    }
+  }
+}

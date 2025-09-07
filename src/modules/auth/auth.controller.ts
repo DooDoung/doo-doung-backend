8c8600b64fd3435a55cd72fc0c73c8aa027220bc
wnmay <wanathachat@gmail.com>
2025-09-07 16:37:02 +0700

    docs: apply log in dto to swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,7 +4,9 @@ import { AuthService } from "./auth.service"
 import { LoginResult } from "./interface/login-result.interface"
 import { LoginDto } from "./dto/login.dto"
 import { HttpCode, HttpStatus } from "@nestjs/common"
+import { ApiTags } from "@nestjs/swagger"
 
+@ApiTags("auth")
 @Controller("auth")
 export class AuthController {
   constructor(private readonly auth: AuthService) {}

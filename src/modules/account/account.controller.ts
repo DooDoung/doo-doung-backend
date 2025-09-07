c98cf91afbf0a761eda6442a2b67f2dfc6175aa1
wnmay <wanathachat@gmail.com>
2025-09-07 11:16:32 +0700

    add: get account dto

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from "@nestjs/common"
 import { AccountService } from "./account.service"
+import { AccountResponseDto } from "./dto/get-account.dto"
 
 @Controller("account")
 export class AccountController {
   constructor(private readonly service: AccountService) {}
 
   @Get()
-  get() {
+  get(): Promise<AccountResponseDto> {
     return this.service.getMyAccount()
   }
 
   @Get(":id")
-  getById(@Param("id") id: string) {
+  getById(@Param("id") id: string): Promise<AccountResponseDto> {
     return this.service.getAccountById(id)
   }
 }

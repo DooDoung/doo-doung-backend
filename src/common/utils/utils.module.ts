7a12d8a999dcc6b0a46fc27a000552d7085b586a
Thananan <thananan.pl@gmail.com>
2025-09-09 23:00:50 +0700

    add: utils module

---
+++ src/common/utils/utils.module.ts
@@ -0,0 +1,10 @@
+// utils/utils.module.ts
+import { Module } from "@nestjs/common"
+import { HashService } from "./hash.service"
+import { GenerateService } from "./generate.service"
+
+@Module({
+  providers: [HashService, GenerateService],
+  exports: [HashService, GenerateService],
+})
+export class UtilsModule {}

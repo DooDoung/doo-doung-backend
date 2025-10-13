a0c19e9dd61bec10796803b737d62e2e0e0b6773
wnmay <wanathachat@gmail.com>
2025-10-14 03:05:13 +0700

    add: implement update payment status

--- src/modules/payment/payment.repository.ts
+++ src/modules/payment/payment.repository.ts
@@ -1,28 +1,37 @@
 import { Injectable } from "@nestjs/common"
-import { PayoutStatus} from "@prisma/client"
+import { PayoutStatus } from "@prisma/client"
 import { PrismaService } from "@/db/prisma.service"
 import { TransactionEntity } from "./interface/transaction.interface"
-import { Decimal } from "@prisma/client/runtime/library";
+import { Decimal } from "@prisma/client/runtime/library"
+import { Tx } from "@/common/types/transaction-client.type"
 
 export interface TransactionCreateInput {
-  id: string;
-  bookingId: string;
-  status: PayoutStatus;
-  amount: Decimal;
+  id: string
+  bookingId: string
+  status: PayoutStatus
+  amount: Decimal
 }
 @Injectable()
 export class PaymentRepository {
-  constructor(
-    private readonly prisma: PrismaService,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async findById(id: string): Promise<TransactionEntity | null> {
     // TO DO: must match owner id (prophet/customer)
-    return this.prisma.transaction.findUnique({ where: { id } });
+    return this.prisma.transaction.findUnique({ where: { id } })
   }
 
   async create(data: TransactionCreateInput): Promise<TransactionEntity> {
-    return this.prisma.transaction.create({ data });
+    return this.prisma.transaction.create({ data })
   }
 
+  async markPayoutPaid(
+    transactionId: string,
+    tx?: Tx
+  ): Promise<TransactionEntity> {
+    const db = tx ?? this.prisma
+    return db.transaction.update({
+      where: { id: transactionId, status: PayoutStatus.PENDING_PAYOUT },
+      data: { status: PayoutStatus.PAID_OUT, updatedAt: new Date() },
+    })
+  }
 }
--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -4,25 +4,35 @@ import { NanoidService } from "@/common/utils/nanoid"
 import { TransactionEntity } from "./interface/transaction.interface"
 import { PayoutStatus } from "@prisma/client"
 import { Decimal } from "@prisma/client/runtime/library"
+import { Tx } from "@/common/types/transaction-client.type"
 
 export interface TransactionCreatePayload {
-  bookingId: string;
-  status: PayoutStatus;
-  amount: Decimal;
+  bookingId: string
+  status: PayoutStatus
+  amount: Decimal
 }
 @Injectable()
 export class PaymentService {
   constructor(
     private readonly repo: PaymentRepository,
-    private readonly nanoidService : NanoidService
+    private readonly nanoidService: NanoidService
   ) {}
 
-  async createPayment(input : TransactionCreatePayload): Promise<TransactionEntity> {
+  async createPayment(
+    input: TransactionCreatePayload
+  ): Promise<TransactionEntity> {
     const id = await this.nanoidService.generateId()
     const data = {
-      id, 
-      ...input
+      id,
+      ...input,
     }
     return this.repo.create(data)
+  }
+
+  async markPayoutPaid(
+    transactionId: string,
+    tx?: Tx
+  ): Promise<TransactionEntity> {
+    return this.repo.markPayoutPaid(transactionId, tx)
   }
 }

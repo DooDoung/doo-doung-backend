5b8570eb30e3f047f56a5aa40c0b183d6f1682ec
Nattarin Chetpattananondh <nattarinchet@gmail.com>
2025-10-13 22:36:35 +0700

    add: get isPublic

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Patch } from "@nestjs/common"
+import { Controller, Get, Param, Patch } from "@nestjs/common"
 import { CustomerService } from "./customer.service"
 import { CurrentUser } from "@/common/decorators/current-user.decorator"
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger"
+import { ApiBearerAuth, ApiTags, ApiOkResponse } from "@nestjs/swagger"
+import { PublicResponseDto } from "./dto/public-response.dto"
+import { Public } from "@/common/decorators/public.decorator"
 
 @ApiTags("customer")
 @Controller("customer")
@@ -9,7 +11,15 @@ export class CustomerController {
   constructor(private readonly service: CustomerService) {}
   @Patch("toggle-public")
   @ApiBearerAuth()
+  @ApiOkResponse({ type: PublicResponseDto })
   togglePublic(@CurrentUser("id") id: string) {
     return this.service.togglePublic(id)
+  }
+
+  @Get("public-status/:id")
+  @Public()
+  @ApiOkResponse({ type: PublicResponseDto })
+  getPublicStatus(@Param("id") id: string): Promise<PublicResponseDto> {
+    return this.service.getPublicStatus(id)
   }
 }
---
+++ src/modules/customer/dto/public-response.dto.ts
@@ -0,0 +1,4 @@
+export class PublicResponseDto {
+  accountId!: string
+  isPublic!: boolean
+}

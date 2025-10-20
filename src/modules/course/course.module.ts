a3ef56ba40e2252eb6dbb7de0a05360ed3c9e4c0
neennera <neennera@gmail.com>
2025-10-20 23:32:48 +0700

    chore: duplicate

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -2,7 +2,6 @@ import { Module, forwardRef } from "@nestjs/common"
 import { CourseController } from "./course.controller"
 import { CourseService } from "./course.service"
 import { CourseRepository } from "./course.repository"
-import { UtilsModule } from "@/common/utils/utils.module"
 import { ProphetModule } from "@/modules/prophet/prophet.module"
 import { UtilsModule } from "@/common/utils/utils.module"
 
